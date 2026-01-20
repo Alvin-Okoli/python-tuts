4367ab6da298270cde37a4a40fa63a7f55d07737
Alvin <alvinokoli@gmail.com>
2026-01-20 02:14:24 +0100

    First Python Tutorial

---
+++ test.py
@@ -0,0 +1,27 @@
+print("Hello WOrld! 🤗")
+print(2 + 2)
+
+message = """""
+    This is a multi-line string.
+    It can span multiple lines.
+"""
+
+course = "Python Programming"
+print(len(course)) 
+
+# Data Types in Python
+x = 10          # Integer
+pi = 3.14           # Float
+is_active = True    # Boolean
+name = "Alice"      # String
+
+# String methods
+greeting = "hello, world!"
+print(greeting.upper())  # Convert to uppercase
+print(greeting.title())  # Convert to title case
+print(greeting.find("world"))  # Find substring
+print(greeting.lower())  # Convert to lowercase
+print(greeting.strip())  # remove whitespace
+print(greeting.replace("o", 'a'))  # replace string
+print("hello" in greeting)  # check substring
+print("hello" not in greeting)  # check substring
